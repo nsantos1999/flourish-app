f33b76cc8fd829978e10fa9218fd7917725f2f91
Natã Souza Santos <nsantos@hitcode.com.br>
2020-05-04 23:04:00 -0300

    Finish

--- src/screens/EnterAccount/index.js
+++ src/screens/EnterAccount/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { 
   KeyboardAvoidingView, 
   Text, 
@@ -8,40 +8,83 @@ import {
   Image } from 'react-native';
 
 import styles from './styles'; 
+import { useDispatch } from 'react-redux';
+
+import * as authActions from '../../store/auth/actions';
+
+import {
+  Button
+} from 'galio-framework';
+
+import theme from '../../theme';
+
+import Logo from '../../components/Logo';
 
 const EnterAccount = ({
   navigation
 }) => {
+  const dispatch = useDispatch();
+
+  const [user, setUser ] = useState({
+    email: '',
+    password: '',
+  })
+
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const canSubmit = () => user.email && user.password;
+
+  const handleSubmit = async () => {
+    const { email, password } = user;
+    setLoading(true);
+    try{
+      await dispatch(authActions.login(email, password));
+    }catch(err) {
+      setError(true);
+    }finally {
+      setLoading(false);
+    }
+  }
+
   return (
     <KeyboardAvoidingView style={styles.background}>
       <View style={styles.containerLogo}>
-        <Image 
+        <Logo />
+        {/* <Image 
         source={require('../../../public/images/flourish.png')} 
-        />
+        /> */}
       </View>
 
       <View style={styles.container}>
+        <Text style={{color: theme.COLORS.ERROR, marginBottom:20}}>{error && 'Login ou senha invalidos'}</Text>
         <TextInput 
-        style={styles.input}
-        placeholder="Email"
-        autoCorrect={false}
-        onChangeText={()=>{}}
+          style={styles.input}
+          placeholder="Email"
+          autoCorrect={false}
+          onChangeText={(value)=>setUser({...user,email: value})}
         />
 
         <TextInput 
-        style={styles.input}
-        placeholder="Password"
-        autoCorrect={false}
-        onChangeText={()=>{}}
+          style={styles.input}
+          placeholder="Senha"
+          autoCorrect={false}
+          onChangeText={(value)=>setUser({...user,password: value})}
         />
 
-      <TouchableOpacity style={styles.btnSubmit} onPress={() => navigation.push('BottomTabs')}>
+        <Button disabled={loading} style={{ backgroundColor: theme.COLORS.WHITE,marginBottom:10 }} textStyle={{ color: theme.COLORS.PRIMARY }} disabled={!canSubmit()} round onPress={handleSubmit}>
+          {loading ? "Autenticando...":"Logar"}
+        </Button>
+        <Button round style={{ backgroundColor: theme.COLORS.PRIMARY}}  onPress={() => navigation.push('SignUp')}>
+            Criar conta
+        </Button>
+
+      {/* <TouchableOpacity style={styles.btnSubmit} onPress={() => navigation.push('BottomTabs')}>
         <Text styles={styles.submitText}>Sign In</Text>
       </TouchableOpacity>
 
       <TouchableOpacity style={styles.btnRegister} onPress={() => navigation.push('SignUp')}>
         <Text styles={styles.registerText}>Create Account</Text>
-      </TouchableOpacity>
+      </TouchableOpacity> */}
 
       </View>
 
